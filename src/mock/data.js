4d3e7a698b6eb7c715e4d7ca92439d7685b99913
Jonathon Gregory <jonathon-gregory@hotmail.com>
2021-05-20 10:06:08 -0500

    changed view

--- src/mock/data.js
+++ src/mock/data.js
@@ -81,8 +81,8 @@ export const projectsData = [
     title: 'Responsive Sign In Page',
     info: `Here is a basic responsive sign in with some JavaScript and CSS animations`,
     info2: `Built with HTML, CSS, and JavaScript `,
-    url: 'https://codepen.io/klf006/pen/vYKxVYB',
-    repo: 'https://codepen.io/klf006/pen/vYKxVYB', // if no repo, the button will not show up
+    url: 'https://codepen.io/klf006/full/vYKxVYB',
+    repo: 'https://codepen.io/klf006/full/vYKxVYB', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
